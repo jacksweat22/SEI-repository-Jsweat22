2c009fcace954a12eaa708ba6167a07ca4d26026
jacksweat22 <suns510@gmail.com>
2020-03-12 14:59:45 -0700

    camlecase error fix

--- events-practice/main.js
+++ events-practice/main.js
@@ -2,6 +2,13 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', function(evt) {
     const li = document.createElement('li');
     const inp = document.querySelector('input');
-    li.textcontent = inp.value;
+    li.textContent = inp.value;
     document.querySelector('ul').appendChild(li);
   });
+
+  document.querySelector('ul')
+    .addEventListener('click', handleClick);
+    
+  function handleClick(evt) {
+      console.log(evt);
+  }
