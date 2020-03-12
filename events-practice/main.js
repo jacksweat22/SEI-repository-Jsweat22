4ca4e8e085cf40ecc30fee8b6a8d2a2c5b5e8959
jacksweat22 <suns510@gmail.com>
2020-03-12 14:43:04 -0700

    corrent event listener

--- events-practice/main.js
+++ events-practice/main.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('button');
-btn.addEventListener('click', function(evt){
-    console.log(event)
-});
-    
+btn.addEventListener('click', function(evt) {
+    const li = document.createElement('li');
+    const inp = document.querySelector('input');
+    li.textcontent = inp.value;
+    document.querySelector('ul').appendChild(li);
+  });
