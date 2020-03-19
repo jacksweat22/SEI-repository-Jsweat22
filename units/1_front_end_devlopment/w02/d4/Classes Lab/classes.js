e79dfbd4528777264bc565022f77166b9837a4e6
jacksweat22 <suns510@gmail.com>
2020-03-19 16:37:30 -0700

    finished classes lab

--- units/1_front_end_devlopment/w02/d4/Classes Lab/classes.js
+++ units/1_front_end_devlopment/w02/d4/Classes Lab/classes.js
@@ -1,3 +1,5 @@
+console.log('hello World')
+
 class Pet {
     constructor(owner, name){
       this.owner = owner;
@@ -6,8 +8,57 @@ class Pet {
     }
     start() {
       this.walk = true;
-      console.log(‘walka walka’);
+      console.log('walka walka');
     }
   }
-  let dog = new Pet (‘Jack’, ‘Simba’);
+  
+  let dog = new Pet ('Jack', 'Simba');
   console.log(dog.start());
+
+  class Dog extends Pet{
+      constructor(owner, name, price){
+      super(owner,name)
+      this.price = price 
+  }
+  bark(){
+      console.log('bark')
+  }
+  chaseTail(){
+      console.log('oh boy oh boy oh boy')
+  }
+  getPrice(){
+      return this.price;
+  }
+}
+
+let doggo =  new Dog ('Jack', 'Gunner', 20)
+console.log(doggo);
+
+class Cat extends Pet{
+    constructor(owner, name, price,){
+    super(owner, name,)
+    this.price = price
+    }
+    purr(){
+        console.log('purrr');
+    }
+
+    clean(){
+        console.log("cleaning");
+    }
+    getPrice(){
+        return this.price
+    }
+    walk(){
+        console.log('strut strut')
+    }
+}
+
+let cat = new Cat ('Jack', 'Cheetah', 10);
+    console.log(cat)
+
+
+// console.log(cat.purr());
+// console.log(cat.clean());
+// console.log(cat.getPrice());
+// console.log(cat.walk());
